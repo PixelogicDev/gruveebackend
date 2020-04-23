6642af8a5d2b093ca6509df3118f48a52751ad52
Alec Dilanchian <alec.dilanchian@gmail.com>
2020-04-22 22:36:55 -0700

    Updating createsocialplaylist && firebase pkg

--- cmd/createsocialplaylist/go.mod
+++ cmd/createsocialplaylist/go.mod
@@ -6,3 +6,21 @@ require (
 	github.com/pixelogicdev/gruveebackend/pkg/firebase v1.0.0-beta.3
 	github.com/pixelogicdev/gruveebackend/pkg/social v1.0.0-beta.2
 )
+
+replace github.com/pixelogicdev/gruveebackend/cmd/socialplatform => ../cmd/socialplatform
+
+replace github.com/pixelogicdev/gruveebackend/cmd/spotifyauth => ../cmd/spotifyauth
+
+replace github.com/pixelogicdev/gruveebackend/cmd/createuser => ../cmd/createuser
+
+replace github.com/pixelogicdev/gruveebackend/pkg/firebase => ../../pkg/firebase
+
+replace github.com/pixelogicdev/gruveebackend/cmd/tokengen => ../cmd/tokengen
+
+replace github.com/pixelogicdev/gruveebackend/cmd/socialtokenrefresh => ../cmd/socialtokenrefresh
+
+replace github.com/pixelogicdev/gruveebackend/pkg/social => ../../pkg/social
+
+replace github.com/pixelogicdev/gruveebackend/cmd/updatealgolia => ../cmd/updatealgolia
+
+replace github.com/pixelogicdev/gruveebackend/cmd/getspotifymedia => ../cmd/getspotifymedia
--- pkg/firebase/firebase.go
+++ pkg/firebase/firebase.go
@@ -48,19 +48,19 @@ type FirestoreMedia struct {
 
 // FirestorePlaylist represents the data for a playlist store in Firestore
 type FirestorePlaylist struct {
-	ID        string        `firestore:"id" json:"id"`
-	Name      string        `firestore:"name" json:"name"`
-	CreatedBy string        `firestore:"createdBy" json:"createdBy"`
-	Members   []string      `firestore:"members" json:"members"`
-	Songs     PlaylistSongs `firestore:"songs" json:"songs"`
-	Comments  interface{}   `firestore:"comments" json:"comments"` // This will actually need be an object with key:value pair of songId:[Comments]
-	CoverArt  string        `firestore:"coverArt" json:"coverArt"`
+	ID        string                   `firestore:"id" json:"id"`
+	Name      string                   `firestore:"name" json:"name"`
+	CreatedBy *firestore.DocumentRef   `firestore:"createdBy" json:"createdBy"`
+	Members   []*firestore.DocumentRef `firestore:"members" json:"members"`
+	Songs     PlaylistSongs            `firestore:"songs" json:"songs"`
+	Comments  interface{}              `firestore:"comments" json:"comments"` // This will actually need be an object with key:value pair of songId:[Comments]
+	CoverArt  string                   `firestore:"coverArt" json:"coverArt"`
 }
 
 // PlaylistSongs represents the songs object in a playlist which includes an AddedBy object and AllSongs DocRef Array
 type PlaylistSongs struct {
-	AddedBy  map[string]string       `firestore:"addedBy" json:"addedBy"`
-	AllSongs []firestore.DocumentRef `firestore:"allSongs" json:"allSongs"`
+	AddedBy  map[string][]string      `firestore:"addedBy" json:"addedBy"`
+	AllSongs []*firestore.DocumentRef `firestore:"allSongs" json:"allSongs"`
 }
 
 // FirestoreEvent implements the Firestore event from a trigger function
